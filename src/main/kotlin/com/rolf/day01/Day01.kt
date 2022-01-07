bad9a39d392eb511e68438ebc59b8214f59cc2f8
Rolf <rolfschellenberger@gmail.com>
2022-01-07 10:23:44 +0100

    Day 1

--- src/main/kotlin/com/rolf/day01/Day01.kt
+++ src/main/kotlin/com/rolf/day01/Day01.kt
@@ -1,6 +1,7 @@
 package com.rolf.day01
 
 import com.rolf.Day
+import com.rolf.util.splitLine
 
 fun main() {
     Day01().run()
@@ -8,11 +9,31 @@ fun main() {
 
 class Day01 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val numbers = splitLine(lines.first()).map { it.toInt() }
+        val circular = numbers + numbers.first()
+
+        val matches = mutableListOf<Int>()
+        for (index in 0..circular.size - 2) {
+            val number = circular[index]
+            if (number == circular[index + 1]) {
+                matches.add(number)
+            }
+        }
+        println(matches.sum())
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val circular = splitLine(lines.first()).map { it.toInt() }
+
+        val matches = mutableListOf<Int>()
+        for (index in circular.indices) {
+            val number = circular[index]
+            val half = (index + circular.size / 2) % circular.size
+            if (number == circular[half]) {
+                matches.add(number)
+            }
+        }
+        println(matches.sum())
     }
 
 }
